053a16c2e64e3af1fda60f23c354ab04c2e89d90
Diego Acosta Corredor <da.acostac1@uniandes.edu.co>
2021-12-09 00:01:41 -0500

    Entrega Final Reto 4

--- App/view.py
+++ App/view.py
@@ -160,7 +160,7 @@ while True:
                 else:
                     ciudad1=lt.getElement(ciudad1,1)
                 origen=True
-                aerosOrigen=controller.buscarAerosCiudad(catalogo,ciudad1)
+                aerosOrigen=controller.buscarAerosCiudad(catalogo,ciudad1['city'])
                 if lt.size(aerosOrigen)>1:
                     print("\nSe encontaron "+str(lt.size(aerosOrigen))+" aeropuertos en "+ciudadOrigen+"\n")
                     aeros={}
@@ -186,7 +186,7 @@ while True:
                 else:
                     ciudad2=lt.getElement(ciudad2,1)
                 destino=True
-                aerosDestino=controller.buscarAerosCiudad(catalogo,ciudad2)
+                aerosDestino=controller.buscarAerosCiudad(catalogo,ciudad2['city'])
                 if lt.size(aerosDestino)>1:
                     print("\nSe encontaron "+str(lt.size(aerosDestino))+" aeropuertos en "+ciudadDestino+"\n")
                     aeros2={}
@@ -201,7 +201,7 @@ while True:
         if ciudad1!=None and ciudad2!=None:
             ruta=controller.rutaMinimaCiudades(catalogo,ciudad1,ciudad2)
         print("\nAeropuerto de salida: "+aerosOrigen["Name"]+"\n\nCiudad: "+aerosOrigen["City"]+"\nPais: "+aerosOrigen["Country"]+"\nLongitud: "+aerosOrigen["Longitude"]+"\nLatitud: "+aerosOrigen["Latitude"]+"\nIATA: "+aerosOrigen["IATA"])
-        print("\nAeropuerto de llegada: "+aerosDestino["Name"]+"\n\nCiudad: "+aerosDestino["City"]+"\nPais: "+aerosDestino["Country"]+"\nLongitud: "+aerosDestino["Longitude"]+"\nLatitud: "+aerosDestino["Latitude"]+"\nIATA: "+aerosDestino["IATA"])
+        print("\nAeropuerto de llegada: "+aerosDestino["Name"]+"\n\nCiudad: "+aerosDestino["City"]+"\nPais: "+aerosDestino["Country"]+"\nLongitud: "+aerosDestino["Longitude"]+"\nLatitud: "+aerosDestino["Latitude"]+"\nIATA: "+aerosDestino["IATA"]+"\n")
     elif int(inputs[0]) == 5:
         millas=int(input("Ingrese las millas disponibles: \n"))
         ciudad=input("Ingrese la ciudad de origen:\n")
